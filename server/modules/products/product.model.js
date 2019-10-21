a20ee9092494ac742d7888e1009f276c782a86eb
Noel Flores <noel.flores@struck.com>
2019-10-21 15:39:51 -0600

    product and category model for mongodb ready

---
+++ server/modules/products/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    title: String,
+    description: String,
+    sizes: [String],
+    price: Number,
+    inventory: Number,
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    }
+});
+
+module.exports = mongoose.Model('Product', productSchema);
